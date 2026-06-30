53214ab21e820885f5a4f84661d76f7455ff36ba
Tristan Krause <tristan.krause@mailbox.tu-dresden.de>
2026-06-30 16:34:15 +0200

    added ULA gain test

--- test/test_ula.cpp
+++ test/test_ula.cpp
@@ -4,10 +4,11 @@
 
 #include <catch2/catch_test_macros.hpp>
 #include <nlohmann/json.hpp>
-
-#include "setup.hpp"
+#include "catch2/catch_approx.hpp"
 #include "components/radiator.hpp"
 #include "math.hpp"
+#include "print.hpp"
+#include "setup.hpp"
 #include "testutil.hpp"
 
 TEST_CASE("ULA position and rotation", "[TestULA]")
@@ -167,7 +168,7 @@ TEST_CASE("ULA gain", "[TestULA]")
     NdArray const rotation_delta = ref_stop.rotation.toNdArray() - ref_start.rotation.toNdArray();
     double const length = pos_delta.norm();
 
-    std::vector<double> gains(n_points, 0.0);
+    std::vector<complex_t> gains(n_points, 0.0);
     std::vector<double> distances(n_points, 0.0);
 
     auto const& sink = setup->get_radiator_by_id("receiver");
@@ -181,14 +182,27 @@ TEST_CASE("ULA gain", "[TestULA]")
         ref_start.pos = pos_start + pos_delta * f;
         ref_start.rotation = rotation_start + rotation_delta * f;
 
-        gains[k] = 0;
-        complex_t gain = 0;
-        for (Radiator const* source : sources) { gain += Radiator::calc_voltage_gain(*source, sink, wavelength); }
-        gains[k] = math::square(std::abs(gain));
+        for (Radiator const* source : sources) { gains[k] += Radiator::calc_voltage_gain(*source, sink, wavelength); }
 
         distance = f * length;
         distances[k] = *distance_ptr;
     }
     ref_start.pos = pos_start;
     ref_start.rotation = rotation_start;
+
+    complex_t const gain_votage_abs_max = std::ranges::max(gains, {}, [](complex_t const& gain) -> double { return std::abs(gain); });
+    REQUIRE(std::abs(gain_votage_abs_max) == Catch::Approx(0.00035809851155573));
+    REQUIRE(std::arg(gain_votage_abs_max) == Catch::Approx(-0.5 * pi).margin(1e-3));
+
+    std::ranges::transform(gains, gains.begin(), [gain_votage_abs_max](auto gain) -> complex_t { return gain / std::abs(gain_votage_abs_max); });
+
+    std::vector<double> const gains_power_expected = {0.100653501560284, 0.227131737832402, 0.430093185362579, 0.684406554078239, 0.90888660875903, 1,
+                                                      0.90888660875903,  0.684406554078239, 0.430093185362579, 0.227131737832402, 0.100653501560284};
+    std::vector<double> const gains_voltage_arg_expected = {-1.78360365074871963, -1.77764329249379149, -1.7634739579509644, -0.33901331302971988, -1.49312253721690102, -1.57131992547758736,
+                                                            -1.49312253721690102, -0.33901331302971988, -1.7634739579509644, -1.77764329249379149, -1.78360365074871963};
+    for (std::size_t k = 0; k < gains.size(); k++)
+    {
+        REQUIRE(math::square(std::abs(gains.at(k))) == Catch::Approx(gains_power_expected.at(k)));
+        REQUIRE(std::arg(gains.at(k)) == Catch::Approx(gains_voltage_arg_expected.at(k)));
+    }
 }
